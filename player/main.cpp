58175727d19921ac4a257f7be0f6b073338af251
Aron Granberg <aron.granberg@gmail.com>
2018-01-12 15:09:28 +0100

    OOP

--- player/main.cpp
+++ player/main.cpp
@@ -6,39 +6,115 @@
 #include <random>
 #include <functional>
 #include <iostream>
+#include <map>
 
 #include "bc.hpp"
 
 using namespace bc;
 using namespace std;
 
-void workerLogic(const GameController& gc, const Unit& unit) {
-    const unsigned id = unit.get_id();
-    Direction d = (Direction) (rand() % 9);
-    // Placing 'em blueprints
-    if(gc.can_blueprint(id, Factory, d) and gc.get_karbonite() > unit_type_get_blueprint_cost(Factory)){
-        cout << "We are building a factory!!" << endl;
-        gc.blueprint(id, Factory, d);
-    } else if (gc.is_move_ready(id) && gc.can_move(id,d)){ // Moving otherwise (if possible)
-        gc.move_robot(id,d);
-    }
-    for (int i = 0; i < 9; ++i) {
-        auto d = (Direction) i;
-        if (gc.can_harvest(id, d)) {
-            gc.harvest(id, d);
-            break;
+GameController gc;
+
+struct BotUnit {
+    Unit unit;
+    const unsigned id;
+    BotUnit(const Unit& unit) : unit(unit), id(unit.get_id()) {}
+    virtual void tick() {}
+};
+
+struct BotWorker : BotUnit {
+    BotWorker(const Unit& unit) : BotUnit(unit) {}
+    void tick() {
+        const unsigned id = unit.get_id();
+        Direction d = (Direction) (rand() % 9);
+        // Placing 'em blueprints
+        if(gc.can_blueprint(id, Factory, d) and gc.get_karbonite() > unit_type_get_blueprint_cost(Factory)){
+            cout << "We are building a factory!!" << endl;
+            gc.blueprint(id, Factory, d);
+        } else if (gc.is_move_ready(id) && gc.can_move(id,d)){ // Moving otherwise (if possible)
+            gc.move_robot(id,d);
+        }
+        for (int i = 0; i < 9; ++i) {
+            auto d = (Direction) i;
+            if (gc.can_harvest(id, d)) {
+                gc.harvest(id, d);
+                break;
+            }
         }
     }
+};
+
+struct BotKnight : BotUnit {
+    BotKnight(const Unit& unit) : BotUnit(unit) {}
+    void tick() {
+        // Calls on the controller take unit IDs for ownership reasons.
+        const auto locus = unit.get_location().get_map_location();
+        const auto nearby = gc.sense_nearby_units(locus, 2);
+        for ( auto place : nearby ){
+            //Building 'em blueprints
+            if(gc.can_build(id, place.get_id()) && unit.get_unit_type() == Worker){
+                gc.build(id, place.get_id());
+                continue;
+            }
+            //Attacking 'em enemies
+            if( place.get_team() != unit.get_team() and
+                    gc.is_attack_ready(id) and
+                    gc.can_attack(id, place.get_id()) ){
+                gc.attack(id, place.get_id());
+                continue;
+            }
+        }
+    }
+};
+
+struct BotRanger : BotUnit {
+    BotRanger(const Unit& unit) : BotUnit(unit) {}
+};
+
+struct BotMage : BotUnit {
+    BotMage(const Unit& unit) : BotUnit(unit) {}
+};
+
+struct BotHealer : BotUnit {
+    BotHealer(const Unit& unit) : BotUnit(unit) {}
+};
+
+struct BotFactory : BotUnit {
+    BotFactory(const Unit& unit) : BotUnit(unit) {}
+
+    void tick() {
+        auto garrison = unit.get_structure_garrison();
+
+        if (garrison.size() > 0){
+            Direction dir = (Direction) (rand() % 8);
+            if (gc.can_unload(id, dir)){
+                gc.unload(id, dir);
+                return;
+            }
+        } else if (gc.can_produce_robot(id, Knight)){
+            cout << "We are producing a Knight!!" << endl;
+            gc.produce_robot(id, Knight);
+            return;
+        }
+    }
+};
+
+struct BotRocket : BotUnit {
+    BotRocket(const Unit& unit) : BotUnit(unit) {}
+};
+
+
+void workerLogic(const GameController& gc, const Unit& unit) {
+
 }
 
 int main() {
     printf("Player C++ bot starting\n");
     printf("Connecting to manager...\n");
-    std::default_random_engine generator;
-    std::uniform_int_distribution<int> distribution (0,8);
-    auto dice = std::bind ( distribution , generator );
+    // std::default_random_engine generator;
+    // std::uniform_int_distribution<int> distribution (0,8);
+    // auto dice = std::bind ( distribution , generator );
     // Most methods return pointers; methods returning integers or enums are the only exception.
-    GameController gc;
     gc.queue_research(Knight);
     gc.queue_research(Knight);
     gc.queue_research(Knight);
@@ -49,6 +125,7 @@ int main() {
         exit(1);
     }
     printf("Connected!\n");
+    map<unsigned int, BotUnit*> unitMap;
 
     // loop through the whole game.
     while (true) {
@@ -73,47 +150,30 @@ int main() {
 
         for (const auto unit : units) {
             const unsigned id = unit.get_id();
+            BotUnit* botUnitPtr;
 
-            if (unit.get_unit_type() == Factory) {
-                auto garrison = unit.get_structure_garrison();
-
-                if (garrison.size() > 0){
-                    Direction dir = (Direction) dice();
-                    if (gc.can_unload(id, dir)){
-                        gc.unload(id, dir);
-                        continue;
-                    }
-                } else if (gc.can_produce_robot(id, Knight)){
-                    cout << "We are producing a Knight!!" << endl;
-                    gc.produce_robot(id, Knight);
-                    continue;
+            if (unitMap.find(id) == unitMap.end()) {
+                switch(unit.get_unit_type()) {
+                    case Worker: botUnitPtr = new BotWorker(unit); break;
+                    case Knight: botUnitPtr = new BotKnight(unit); break;
+                    case Ranger: botUnitPtr = new BotRanger(unit); break;
+                    case Mage: botUnitPtr = new BotMage(unit); break;
+                    case Healer: botUnitPtr = new BotHealer(unit); break;
+                    case Factory: botUnitPtr = new BotFactory(unit); break;
+                    case Rocket: botUnitPtr = new BotRocket(unit); break;
+                    default:
+                        cerr << "Unknown unit type!" << endl;
+                        exit(1);
                 }
+                unitMap[id] = botUnitPtr;
+            } else {
+                botUnitPtr = unitMap[id];
             }
 
-            if (unit.get_location().is_on_map()){
-            // Calls on the controller take unit IDs for ownership reasons.
-                const auto locus = unit.get_location().get_map_location();
-                const auto nearby = gc.sense_nearby_units(locus, 2);
-                for ( auto place : nearby ){
-                    //Building 'em blueprints
-                    if(gc.can_build(id, place.get_id()) && unit.get_unit_type() == Worker){
-                        gc.build(id, place.get_id());
-                        continue;
-                    }
-                    //Attacking 'em enemies
-                    if( place.get_team() != unit.get_team() and
-                            gc.is_attack_ready(id) and
-                            gc.can_attack(id, place.get_id()) ){
-                            gc.attack(id, place.get_id());
-                            continue;
-                    }
-                }
-            }
-
-            if (unit.get_unit_type() == Worker) {
-                workerLogic(gc, unit);
-            }
+            BotUnit& botUnit = *botUnitPtr;
+            botUnit.tick();
         }
+
         // this line helps the output logs make more sense by forcing output to be sent
         // to the manager.
         // it's not strictly necessary, but it helps.
