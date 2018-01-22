4ec28849efc32ba70736b426b9eab2842870bf03
Simon Lindholm <simon.lindholm10@gmail.com>
2018-01-22 18:00:08 +0100

    Deploy script fixes

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,6 @@ rm -rf _deploy/
 cp -rL player/ _deploy/
 
 HASH=$(git rev-parse HEAD)
-sed -i "s/BC_DEPLOY=0/BC_DEPLOY=1; COMMIT_HASH=$HASH/" _deploy/run.sh
+sed -i "s/BC_DEPLOY=0/BC_DEPLOY=1; COMMIT_HASH=$HASH/" _deploy/build.sh
 
 rm -f _deploy/main _deploy/*.o
