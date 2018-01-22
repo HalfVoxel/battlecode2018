9f89bcbcbcfedaea129609547ee0098bbfc9c410
Simon Lindholm <simon.lindholm10@gmail.com>
2018-01-22 20:01:40 +0100

    build.sh changes

--- player/build.sh
+++ player/build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 # build the program!
 
+set -e
+
 if [ "$BC_PLATFORM" = 'LINUX' ]; then
     LIBRARIES="-lbattlecode-linux -lutil -ldl -lrt -pthread -lgcc_s -lc -lm -L../battlecode/c/lib"
     INCLUDES="-I../battlecode/c/include -I."
@@ -13,20 +15,21 @@ else
     exit 1
 fi
 
+step() {
+    echo $@
+    $@
+}
+
 DEPLOY_CC='g++ -std=c++11 -O2 -g -rdynamic -DCUSTOM_BACKTRACE -fno-omit-frame-pointer -no-pie'
 
 BC_DEPLOY=0
 if [ "$BC_DEPLOY" = '2' ]; then
     # We run out of memory with g++. Instead we ship a pre-compiled binary, and just link it in this step.
-    echo $DEPLOY_CC everything.o -o main $LIBRARIES
-    $DEPLOY_CC everything.o -o main $LIBRARIES
+    step $DEPLOY_CC everything.o -o main $LIBRARIES
     echo starting
 elif [ "$BC_DEPLOY" = '1' ]; then
-    echo $DEPLOY_CC everything.cpp -c $INCLUDES
-    $DEPLOY_CC everything.cpp -c $INCLUDES
-    echo $DEPLOY_CC everything.o -o main $LIBRARIES
-    $DEPLOY_CC everything.o -o main $LIBRARIES
+    step $DEPLOY_CC everything.cpp -c $INCLUDES
+    step $DEPLOY_CC everything.o -o main $LIBRARIES
 else
-    echo g++ -std=c++11 -O2 -Wall -g -rdynamic everything.cpp -DBACKTRACE -o main $LIBRARIES $INCLUDES
-    g++ -std=c++11 -O2 -Wall -g -rdynamic everything.cpp -fsanitize=address -o main $LIBRARIES $INCLUDES
+    step g++ -std=c++11 -O2 -Wall -g -rdynamic everything.cpp -DBACKTRACE -o main $LIBRARIES $INCLUDES
 fi
