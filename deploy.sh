5bf06e4bfec2bde195579e58b62da44f173e6e40
Simon Lindholm <simon.lindholm10@gmail.com>
2018-01-22 17:37:29 +0100

    Small deploy script change

--- deploy.sh
+++ deploy.sh
@@ -8,4 +8,4 @@ cp -rL player/ _deploy/
 HASH=$(git rev-parse HEAD)
 sed -i "s/BC_DEPLOY=0/BC_DEPLOY=1; COMMIT_HASH=$HASH/" _deploy/run.sh
 
-rm -f _deploy/main _deploy/everything.o
+rm -f _deploy/main _deploy/*.o
