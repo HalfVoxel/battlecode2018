51f1b224346230e6f608f96b18f78524a43661bc
Aron Granberg <aron.granberg@gmail.com>
2018-01-30 18:01:23 +0100

    Karbonite group size = 30

--- player/worker.cpp
+++ player/worker.cpp
@@ -20,7 +20,7 @@ struct KarboniteGroup {
 vector<KarboniteGroup> groupKarbonite() {
     vector<vector<bool>> covered(w, vector<bool>(h));
     vector<vector<int>> groupIndices(w, vector<int>(h, -1));
-    int maxTimeCostPerGroup = 1000;
+    int maxTimeCostPerGroup = 30;
     vector<KarboniteGroup> groups;
 
     for (int x = 0; x < w; x++) {
